2f428d47a17f66baf3b6025016f5488b41c03985
wither7007 <jimsteffes@outlook.com>
2022-02-24 22:26:44 -0600

    ok

---
+++ build/precache-manifest.b437dd11366ad8af39af461f550ee20c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2cb697089b074a37841d07c7b18e07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "139bf9ddb6f7d31a804d",
+    "url": "/static/css/main.018e9ea7.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139bf9ddb6f7d31a804d",
+    "url": "/static/js/main.b1d25d62.chunk.js"
+  },
+  {
+    "revision": "4aad21eb92ea52ff6838",
+    "url": "/static/js/runtime-main.9adee5b9.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
